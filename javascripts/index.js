6f4e78af86c8c393a48f6c5ebb7c1750921d907b
mccleskeyc <mccleskeyc@gmail.com>
2021-02-25 11:45:54 -0500

    added form to create a new event

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,38 @@
-
 //grab and return element "main"
 function main() {
     return document.getElementById("main")
+}
+
+//grab main using main(), then clear it
+function resetMain() {
+    main().innerHTML = ""
+}
+
+//render the form to create an event
+function formTemplate() {
+    return `
+    <h3>Create Event</h3>
+        <form id="form">
+            <div class="input-field">
+            <label for="meeting">Event:</label>
+            <input type="text" name="meeting" id="meeting">
+            </div>
+
+            <br>
+
+            <div class="input-field">
+                <label for="description">Description:</label>
+                <textarea name="description" id="description" cols="30" rows="10"></textarea>
+            </div>
+
+            <br>
+
+            <div class="input-field">
+                <label for="date">Date:</label>
+                <input type="date" name="date" id="date">
+            </div>
+
+            <input type="submit" value="Create an Event">
+        </form>
+    `;
 }
