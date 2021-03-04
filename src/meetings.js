dbe97d114589c140a8b9bc4038e7ce11023e68af
mccleskeyc <mccleskeyc@gmail.com>
2021-03-04 15:42:29 -0500

    created Meeting class and added constructor

---
+++ src/meetings.js
@@ -0,0 +1,15 @@
+class Meeting {
+
+    static all = []
+
+    constructor(id, name, location, date, description, hostName) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.date = date;
+        this.description = description;
+        this.hostName = hostName;
+        Meeting.all.push(this)
+        //pushes new obj into array all from static method with all above info
+    }
+}
